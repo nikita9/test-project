050722bfad95d1ac8e5c6eed5481d9045c0bcc7a
nikita9 <nikita9@ua.fm>
2017-06-28 23:11:13 +0300

    add allure-report

--- config/protractor.conf.ts
+++ config/protractor.conf.ts
@@ -13,6 +13,15 @@ let conf: Config = {
   baseUrl: 'http://www.google.com/',
 
   onPrepare: () => {
+    exports.config = {
+  framework: 'jasmine2',
+  onPrepare: function() {
+    var AllureReporter = require('jasmine-allure-reporter');
+    jasmine.getEnv().addReporter(new AllureReporter({
+      resultsDir: 'allure-results'
+    }));
+  }
+}
     // Adding nice console output. 
     // Provided by: https://github.com/razvanz/jasmine2-reporter
     let ConsoleReporter = require('jasmine2-reporter').Jasmine2Reporter
