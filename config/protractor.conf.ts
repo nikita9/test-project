124e1cf74aaf90578c76477236461fe406d1b3e3
nikita9 <nikita9@ua.fm>
2017-07-06 17:36:12 +0300

    Update protractor.conf.ts
    var AllureReporter = require('jasmine-allure-reporter');
    jasmine.getEnv().addReporter(new AllureReporter({
      resultsDir: 'allure-results'
    }));

--- config/protractor.conf.ts
+++ config/protractor.conf.ts
@@ -13,6 +13,10 @@ let conf: Config = {
   baseUrl: 'http://www.protractortest.org/testapp/ng1/',
 
   onPrepare: () => {
+    var AllureReporter = require('jasmine-allure-reporter');
+    jasmine.getEnv().addReporter(new AllureReporter({
+      resultsDir: 'allure-results'
+    }));
     // Adding nice console output. 
     // Provided by: https://github.com/razvanz/jasmine2-reporter
     let ConsoleReporter = require('jasmine2-reporter').Jasmine2Reporter
